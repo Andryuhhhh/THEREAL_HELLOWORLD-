d28d6eda8c6ae5c4279afeb16a59eb5bc9bab77c
Andrey <b687po@gmail.com>
2026-09-21 15:24:49 +0300

    upd main

---
+++ fron.py
@@ -0,0 +1,8 @@
+def helloworld(a):
+    try:
+        if a == "print":
+            print("Hello World!")
+        else:
+            raise ValueError("Not Hello World!")
+    except ValueError:
+        print("error")
